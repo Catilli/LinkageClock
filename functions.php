3fe667feb8c3b48a35aec5ccecffab9f3e58ef15
Catilli <hello.cath.dev@gmail.com>
2025-08-09 02:56:35 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 // Include create-table.php functionality
 require_once get_template_directory() . '/functions/create-table.php';
 
-// Include enqueue.php for scripts and styles
-require_once get_template_directory() . '/functions/enqueue.php';
-
 // Include custom-roles.php for role management
 require_once get_template_directory() . '/functions/custom-roles.php';
+
+// Include theme-setup.php for theme support and setup
+require_once get_template_directory() . '/functions/theme-setup.php';
