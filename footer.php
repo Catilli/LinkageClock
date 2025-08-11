6703a18e6ec24fdb4e28c889e73e73ce41876057
Catilli <hello.cath.dev@gmail.com>
2025-08-11 22:08:48 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,7 @@
         </main><!-- #main -->
     </div><!-- #content -->
 
-    <footer id="colophon" class="site-footer bg-gray-800 text-white mt-12">
-        <div class="container mx-auto px-4 py-8">
-        </div>
-    </footer>
+    <footer id="colophon"></footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
